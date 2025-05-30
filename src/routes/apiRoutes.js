ce1c60ee2cdcbbc7fcb74254fa679b20ef1c20cd
gyan9427 <gyan9427@gmail.com>
2025-05-30 17:03:18 +0530

    add login routes and controller

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import staffListRoutes from './StaffListRoutes.js';
 import seedRoutes from './SeedRoutes.js';
+import loginRoutes from "./loginRoutes.js"
 
 const router = Router();
 
 router.use('/staff', staffListRoutes);
 router.use('/seed', seedRoutes);
+router.use('/login', loginRoutes)
 
 export default router;
