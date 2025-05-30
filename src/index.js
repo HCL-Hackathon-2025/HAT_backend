969baf13c658e5dfd8751dde3913c662e5faeb26
gyan9427 <gyan9427@gmail.com>
2025-05-30 13:29:40 +0530

    dev: create basic feature and files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json({
+    limit: '5mb' 
+}));
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
