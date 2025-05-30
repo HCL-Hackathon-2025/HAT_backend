f2029dd17c9064fb6077c3c481fcd29e042adc6e
gyan9427 <gyan9427@gmail.com>
2025-05-30 18:14:04 +0530

    code refactor

--- src/controllers/StaffListController.js
+++ src/controllers/StaffListController.js
@@ -30,7 +30,7 @@ export const addStaffMember = async(req, res) => {
     const { role = "admin" } = req.user || {};
 
 
-    if (role !== 'admin' && role !== 'hr') {
+    if (role !== 'admin' ) {
         return res.status(403).json({ message: "Forbidden: You do not have permission to access this resource." });
     }
 
